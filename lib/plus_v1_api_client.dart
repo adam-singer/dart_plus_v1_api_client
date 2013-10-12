00413316d7484cdd71f84706fc5abb551b81bc07
Adam Singer <financeCoding@gmail.com>
2013-10-12 00:35:29 -0700

    updating

---
+++ lib/plus_v1_api_client.dart
@@ -0,0 +1,13 @@
+library plus_v1_api;
+
+import "dart:core" as core;
+import "dart:async" as async;
+import "dart:json" as JSON;
+import 'dart:collection' as dart_collection;
+
+import 'package:google_plus_v1_api/src/client_base.dart';
+export 'package:google_plus_v1_api/src/client_base.dart' show APIRequestError;
+
+part 'src/client/client.dart';
+part 'src/client/schemas.dart';
+part 'src/client/resources.dart';
